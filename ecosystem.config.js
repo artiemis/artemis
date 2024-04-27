248243770c0c4fad430f32bb9fb9756117644a6e
artie <artiehime@gmail.com>
2024-04-27 12:06:49 +0200

    use python directly

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,8 @@ module.exports = {
   apps: [
     {
       name: "artemis",
-      script: "/bin/bash",
-      args: "-c './env/bin/python -m artemis.bot'",
+      script: "./env/bin/python",
+      args: "-m artemis.bot",
       out_file: "/dev/null",
       error_file: "/dev/null",
       log_file: "./artemis.log",
